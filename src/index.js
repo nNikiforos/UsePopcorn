ce507ae6fc50fb6e10bf2041c9e6cf26377ca04d
nNikiforos <nikiforos.nik96@gmail.com>
2024-08-31 11:57:09 +0300

    usepop corn app finished

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
-      messages={["Terrible", "bad", "okey", "good", "amazing"]}
-      defaultRating={2}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test /> */}
   </React.StrictMode>
 );
